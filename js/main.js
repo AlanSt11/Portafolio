8c5a7ffb1d13da9e863ef90820c8e452cc48f958
AlanSt11 <86734556+AlanSt11@users.noreply.github.com>
2021-12-02 12:11:30 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,77 @@
+const enlaces = document.getElementsByClassName("nav__list")[0];
+const hamburgesa = document.getElementsByClassName("nav__icon")[0];
+const menuHamburgesa = document.getElementById("nav__icon");
+let abierto = false;
+
+/*MENU*/
+const toggleMenu = () =>{
+    enlaces.classList.toggle("nav__list2");
+    enlaces.style.transition = "transform 0.5s ease-in-out";
+}
+
+hamburgesa.addEventListener("click", function(){
+    toggleMenu();
+    if(document.querySelector(".nav__list.nav__list2")){
+        abierto = true;
+    } else {
+        abierto = false;
+    }
+})
+
+
+
+window.addEventListener("resize", function(){
+    if(screen.width > 800){
+        if(abierto){
+            toggleMenu();
+            enlaces.style.transition = "none";
+            abierto = false
+        }
+    }
+})
+
+/*EFECTO SCROLL*/
+const typed = new Typed('.typed', {
+    strings: ['<i class="title">Diseñador web</i>', '<i class="title">Desarollador de aplicaciones moviles</i>'],
+
+	typeSpeed: 75,
+	startDelay: 300,
+	backSpeed: 75,
+	shuffle: false,
+	backDelay: 1500,
+	loop: true,
+	loopCount: false,
+	showCursor: true,
+	cursorChar: '|',
+	contentType: 'html',
+})
+
+ScrollReveal().reveal('.hero');
+ScrollReveal().reveal('.nav__logo');
+ScrollReveal().reveal('nav__item');
+ScrollReveal().reveal('nav__link');
+ScrollReveal().reveal('hero__main container');
+ScrollReveal().reveal('.hero__text');
+ScrollReveal().reveal('.imagen__portada');
+ScrollReveal().reveal('.hero__title');
+ScrollReveal().reveal('.hero__waves');
+ScrollReveal().reveal('.title__proyects');
+ScrollReveal().reveal('.proyects');
+ScrollReveal().reveal('.proyects__descrition');
+ScrollReveal().reveal('.proyects__img');
+ScrollReveal().reveal('.txt__proyects');
+ScrollReveal().reveal('.link__proyect1');
+ScrollReveal().reveal('.title__skills');
+ScrollReveal().reveal('.skills');
+ScrollReveal().reveal('.img__skilss');
+ScrollReveal().reveal('.footer__contacto');
+ScrollReveal().reveal('.footer_imgContacto');
+ScrollReveal().reveal('.gmail');
+ScrollReveal().reveal('.Red__social');
+ScrollReveal().reveal('.derechos');
+
+/*EFECTO LOADER*/
+window.addEventListener("load", function(e){
+    e.preventDefault();
+    document.getElementById("loader").classList.toggle("loader2")
+})
